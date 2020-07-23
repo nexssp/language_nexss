b66c89f7e3b7647797f32379d7b58ce8a5b53679
Marcin Polak <mapoart@gmail.com>
2020-07-23 12:50:32 +0200

    Added some more info

--- nexss.win32.nexss.config.js
+++ nexss.win32.nexss.config.js
@@ -3,6 +3,9 @@ languageConfig.title = "Nexss";
 languageConfig.description =
   "Nexss is an innovative and evolutionary tool for the programmers.";
 languageConfig.url = "https://nexss.com";
+languageConfig.founders = ["Marcin Polak"];
+languageConfig.developers = ["Nexss.com"];
+languageConfig.years = ["2018"];
 languageConfig.extensions = [".nexss"];
 languageConfig.executeCommandLine = "";
 languageConfig.printCommandLine = ""; //no console.log() needed to display result eg node -p "4+6"
@@ -15,8 +18,8 @@ languageConfig.compilers = {
     // Cpp does not have possibility to compile and run on the fly. We need to save it as a exe file first.
     command: "nexss",
     args: "<file> --nexssScript",
-    help: ``
-  }
+    help: ``,
+  },
 };
 languageConfig.errors = require("./nexss.nexss.errors");
 languageConfig.languagePackageManagers = {};
