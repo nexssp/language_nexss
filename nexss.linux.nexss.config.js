71f3abcc02b0e3193841f01a0a758ffb5fa0a791
Marcin Polak <mapoart@gmail.com>
2020-08-31 16:45:08 +0200

    Linux adjustments

--- nexss.linux.nexss.config.js
+++ nexss.linux.nexss.config.js
@@ -1,11 +1,8 @@
-let languageConfig = Object.assign({}, require("../config.win32"));
+let languageConfig = Object.assign({}, require(`../config.linux`));
 languageConfig.title = "Nexss";
 languageConfig.description =
   "Nexss is an innovative and evolutionary tool for the programmers.";
 languageConfig.url = "https://nexss.com";
-languageConfig.founders = ["Marcin Polak"];
-languageConfig.developers = ["Nexss.com"];
-languageConfig.years = ["2018"];
 languageConfig.extensions = [".nexss"];
 languageConfig.executeCommandLine = "";
 languageConfig.printCommandLine = ""; //no console.log() needed to display result eg node -p "4+6"
@@ -15,7 +12,6 @@ languageConfig.builders = {};
 languageConfig.compilers = {
   nexss: {
     install: "installed.",
-    // Cpp does not have possibility to compile and run on the fly. We need to save it as a exe file first.
     command: "nexss",
     args: "<file> --nexssScript",
     help: ``,
